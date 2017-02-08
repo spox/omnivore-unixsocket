b0af8be7d14726d80e6cf54abcaea4c416ebd1c3
Chris Roberts <code@chrisroberts.org>
2017-02-07 20:30:23 -0800

    Updates for crystal version

--- spec/omnivore-unixsocket_spec.cr
+++ spec/omnivore-unixsocket_spec.cr
@@ -11,7 +11,7 @@ describe Omnivore::Source::Unixsocket do
     app = Omnivore::Application::Pathed.new(config)
     endpoint = app.endpoints["tester"]
     source = endpoint.sources.first
-    final = app.endpoints["spec"].sources.first as Omnivore::Source::Spec
+    final = app.endpoints["spec"].sources.first.as(Omnivore::Source::Spec)
     app.consume!
     spawn do
       100.times do
